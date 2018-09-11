1b38977f83d7bab2e27d1fddac9f36afee45aeea
Ben Awad <benawad97@gmail.com>
2018-09-11 15:33:25 -0500

    create some components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const Body = () => (
+  <p className="App-intro">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+);
+// const Body = () => {
+//   return (
+//     <p className="App-intro">
+//       To get started, edit <code>src/App.js</code> and save to reload.
+//     </p>
+//   );
+// };
+
+// function Body() {
+//   return (
+//     <p className="App-intro">
+//       To get started, edit <code>src/App.js</code> and save to reload.
+//     </p>
+//   );
+// }
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">hi</h1>
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Body />
       </div>
     );
   }
