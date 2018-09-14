6d21280c92a8f34403cb0b1b9ffe51999aa5de2b
Ben Awad <benawad97@gmail.com>
2018-09-14 10:56:01 -0500

    make a counter to practice state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Body, { Body3, Body2 } from "./components/Body";
 import Header from "./components/Header";
+import Counter from "./components/Counter";
 
 class App extends Component {
   add(a, b) {
@@ -23,7 +24,8 @@ class App extends Component {
         />
         <Body2 />
         <Body myFunc={this.add} text="i am cool" text2="i am cool2" />
-        <Body myFunc={this.add} text="i am" text2="i cool2" />
+        <Counter initialCount={0} />
+        <Counter initialCount={10} />
       </div>
     );
   }
---
+++ src/components/Counter.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+      </div>
+    );
+  }
+}
