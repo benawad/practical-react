6fd161485e4fbb75c59dc8b1fac7f08eef6b587f
Ben Awad <benawad97@gmail.com>
2018-09-23 11:12:36 -0500

    lifting up state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
-import FetchRandomUser from "./components/FetchRandomUser";
+import Counter from "./components/Counter";
 
 class App extends Component {
   state = {
-    visible: true
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <FetchRandomUser />
+        <Counter
+          count={this.state.count}
+          decrement={this.decrement}
+          increment={this.increment}
+        />
+        <Counter
+          count={this.state.count}
+          decrement={this.decrement}
+          increment={this.increment}
+        />
       </div>
     );
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,36 +1,12 @@
 import React from "react";
 
 export default class Counter extends React.Component {
-  state = {
-    count: 0
-  };
-
-  componentWillUnmount() {
-    console.log("unmounting...");
-  }
-
-  componentDidMount() {
-    console.log("mounting...");
-  }
-
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  };
-
-  decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  };
-
   render() {
     return (
       <div>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.increment}>increment</button>
-        <button onClick={this.decrement}>decrement</button>
+        <div>count: {this.props.count}</div>
+        <button onClick={this.props.increment}>increment</button>
+        <button onClick={this.props.decrement}>decrement</button>
       </div>
     );
   }
